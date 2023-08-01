eaefad6a26eafe3d9103fb3b26258b1f1bb33b1c
geeklive123 <keima13234@hotmail.com>
2023-07-31 22:50:59 -0400

    Inicio de Proyecto

---
+++ Backend/config/config.js
@@ -0,0 +1,14 @@
+const mysql=require('mysql');
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'udemy_delivery'
+})
+
+db.connect(function(err){
+    if(err) throw err;
+    console.log('DATABASE CONNECTED!');
+});
+module.exports=db;
+
