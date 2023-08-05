934692b48d1052ab666112504d32b3e042abe107
geeklive123 <keima13234@hotmail.com>
2023-08-04 20:22:16 -0400

    Captura de datos de la pestaña de login

--- Front-End/src/views/home/Home.tsx
+++ Front-End/src/views/home/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useState}from 'react';
 import {useNavigation} from '@react-navigation/native'
 import { StyleSheet, Text, View, Image, TextInput,Button, ToastAndroid, TouchableOpacity } from "react-native";
 import { RoundedButton } from "../../components/RoundedButton";
 import {StackNavigationProp} from '@react-navigation/stack'
 import { RootStackParamList } from '../../../App';
 export const HomeScreen = () => {
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
     const navigation=useNavigation<StackNavigationProp<RootStackParamList>>()
     return (
         <View style={styles.container}>
@@ -27,6 +29,8 @@ export const HomeScreen = () => {
                 placeholder="Correo Electronico"
                 style={styles.formTextInput}
                 keyboardType="email-address"
+                value={email}
+                onChangeText={text=>setEmail(text)}
               />
             </View>
             <View style={styles.formInput}>
@@ -39,10 +43,15 @@ export const HomeScreen = () => {
                 style={styles.formTextInput}
                 keyboardType="default"
                 secureTextEntry={true}
+                value={password}
+                onChangeText={text=>setPassword(text)}
               />
             </View>
             <View style={{marginTop:30}}>
-            <RoundedButton onPress={()=>ToastAndroid.show('Hola',ToastAndroid.SHORT)} text='ENTRAR' />
+            <RoundedButton onPress={()=>{
+              console.log('Email: ',+email);
+              console.log('Password: ',+ password);
+            }} text='ENTRAR' />
             </View>
             <View style={styles.formRegister}>
           <Text>No Tienes Cuenta</Text>
